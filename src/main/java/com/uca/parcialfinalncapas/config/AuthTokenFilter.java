7f621ebe5dbe73b35b573772f60d25f79c4665d7
AlisonArgue <00076422@uca.edu.sv>
2025-07-08 19:41:57 -0600

    :sparkles: add security

---
+++ src/main/java/com/uca/parcialfinalncapas/config/AuthTokenFilter.java
@@ -0,0 +1,16 @@
+package com.uca.parcialfinalncapas.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.authentication.*;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class AuthTokenFilter {
+}
